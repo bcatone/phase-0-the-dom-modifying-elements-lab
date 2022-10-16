e418aa937d446bd7b88c3970917ed7d5a5fb7f36
Brittany Laura Catone <bcatone@outlook.com>
2022-10-15 23:29:37 -0400

    commit tree 899d68814db8a00fa9b113ed039a5a43392b323d

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const element = document.getElementById("main");
+document.body.removeChild(element);
+
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.textContent = "Brittany is the champion"
+document.body.append(newHeader);
